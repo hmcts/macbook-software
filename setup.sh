cb177917ba09845e7639d2be80f189f36097b758
Sandy Gudgeon <alexander.gudgeon@justice.gov.uk>
2021-02-08 13:54:12 +0000

    Update setup.sh
    V0.3 by Alexander Gudgeon
    added options to install data for HMCTS Data Engineers as well as DTS Developers/QAs

--- setup.sh
+++ setup.sh
@@ -1,9 +1,111 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+#software for DTS Developers
+developers(){
+# Collection of packages to install
+PACKAGES=(
+    git
+    npm
+    postgresql
+    python3
+    azure-cli
+)
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+echo "Cleaning up..."
+brew cleanup
+#now install casks
+CASKS=(
+    docker
+    adoptopenjdk
+    intellij-idea
+    pycharm
+)
+echo "Installing cask apps..."
+brew install --cask --force ${CASKS[@]}
+echo "Cleaning up..."
+brew cleanup
+}
+
+#software for A&P Data Engineers
+dataengineers(){
+# Collection of packages to install
+PACKAGES=(
+    git
+    npm
+    postgresql
+    python3
+    azure-cli
+)
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+echo "Cleaning up..."
+brew cleanup
+#now install casks
+CASKS=(
+    docker
+    pycharm
+    azure-data-studio
+    microsoft-azure-storage-explorer
+    R
+    rstudio
+)
+echo "Installing cask apps..."
+brew install --cask --force ${CASKS[@]}
+echo "Cleaning up..."
+brew cleanup
+}
+
+#all software
+allsoftware(){
+# Collection of packages to install
+PACKAGES=(
+    git
+    npm
+    postgresql
+    python3
+    azure-cli
+)
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+echo "Cleaning up..."
+brew cleanup
+#now install casks
+CASKS=(
+    adoptopenjdk
+    intellij-idea
+    docker
+    pycharm
+    azure-data-studio
+    microsoft-azure-storage-explorer
+    R
+    rstudio
+)
+echo "Installing cask apps..."
+brew install --cask --force ${CASKS[@]}
+echo "Cleaning up..."
+brew cleanup
+}
+
+ # Install homebrew if not already installed
+install_homebrew(){
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+# Update homebrew
+brew update
+#cask install no longer needed as cask is part of homebrew
+#echo "Installing homebrew-cask..."
+#brew install caskroom/cask/brew-cask   
+}
+
+clear
 # Bootstrap script for setting up a new OSX machine
 # for Digital Delivery devs and QAs.
 
-
-# Version 0.2 - Made by Thomas Geraghty
+# Version 0.2 - Thomas Geraghty
+# Version 0.3 - Sandy Gudgeon
 
 cat << "EOF"
  _   _ ___  ________ _____ _____   _____________
@@ -17,53 +119,39 @@ cat << "EOF"
 |  |  | | | |__   |     Inspiring change and lookin' cool   
 |____/  |_| |_____|     whilst we do it!                    
 EOF
-
-echo ''
-echo "Beginning bootstrap installation script!"
-sleep 1s
-
-# Install homebrew if not already installed
-if test ! $(which brew); then
-    echo "Installing homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
-
-# Update homebrew
-brew update
-
-echo "Installing homebrew-cask..."
-brew install caskroom/cask/brew-cask
-
-# Collection of packages to install
-PACKAGES=(
-    git
-    npm
-    postgresql
-    python3
-    azure-cli
-)
-
-echo "Installing packages..."
-brew install ${PACKAGES[@]}
-
-echo "Cleaning up..."
-brew cleanup
-
-# Could install apps from Self Service from here instead?
-# If one of these is already installed, the install op will fail and miss any other packages. 
-CASKS=(
-   
-    adoptopenjdk
-    intellij-idea
-    pycharm
-    azure-data-studio
-    datagrip
-)
-
-echo "Installing cask apps..."
-brew install --cask ${CASKS[@]}
-
-echo "Cleaning up..."
-brew cleanup
-
-echo "Bootstrapping complete"
+echo
+echo "DTS Macbook Development Tools Installer V0.3"
+echo ""
+echo "Any problems email :"
+echo "Thomas Geraghty - thomas.geraghty@justice.gov.uk for DTS Developer/QA queries"
+echo "Alexander Gudgeon - alexander.gudgeon@justice.gov.uk for A&P Data Engineering queries"
+echo ""
+ECHO "Pease enter your choice: "
+options=("DTS Developer Tools" "A&P Data Engineer Tools" "All Tools" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "DTS Developer Tools")
+            echo "you chose to install tools for $opt - $REPLY"
+            install_homebrew
+            developers
+            break
+            ;;
+        "A&P Data Engineer Tools")
+            echo "you chose to install tools for $opt - $REPLY"
+            install_homebrew
+            dataengineers
+            break
+            ;;
+        "All Tools")
+            echo "you chose choice to install $opt - $REPLY"
+            install_homebrew
+            allsoftware
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
